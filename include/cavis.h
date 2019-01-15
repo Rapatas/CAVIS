6c67ac1a69b58626499a1b0a45424a26ef847993
Rapatas <15367779+Rapatas@users.noreply.github.com>
2019-01-15 11:11:10 +0200

    Changed Cavis to a template

--- include/cavis.h
+++ include/cavis.h
@@ -8,9 +8,8 @@
 #include "pixels.h"
 #include "grid.h"
 
+template <class Automaton>
 class Cavis : public sf::Drawable, public sf::Transformable {
-
-	CellularAutomaton *automaton;
 
 	unsigned width;
 	unsigned height;
@@ -21,21 +20,93 @@ class Cavis : public sf::Drawable, public sf::Transformable {
 	Pixels pixels;
 	std::vector<Grid> grids;
 
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	Automaton automaton;
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+
+		states.transform *= getTransform();
+		target.draw(pixels, states);
+
+		if (show_grid) {
+			for (const auto &g : grids) {
+				target.draw(g, states);
+			}
+		}
+	}
 
 public:
 
 	Cavis(
-		CellularAutomaton *automaton,
 		unsigned width,
 		unsigned height,
 		unsigned pixel_size = 5
-	);
+	) :
+		width(width),
+		height(height),
+		pixel_size(pixel_size),
+		show_grid(true),
+		pixels({width, height}),
+		automaton(sf::Vector2u(width, height))
+	{
+		pixels.scale(pixel_size, pixel_size);
+		automaton.set_dimentions({width, height});
+	}
 
-	void update(double dt);
-	void handle_user();
-	void add_grid(unsigned size, sf::Color color);
-	void set_dimentions(sf::Vector2u dim);
+	void update(double dt) {
+
+		static double accumulator = 0;
+		accumulator += dt;
+
+		while (accumulator > 1 / steps_per_sec) {
+			automaton.step();
+			accumulator -= 1 / steps_per_sec;
+		}
+
+		for (unsigned i = 0; i != width * height; ++i) {
+			pixels.set_pixel(i, automaton.get_pixel(i));
+		}
+
+		std::vector<Agent> agents = automaton.get_agents();
+
+		for (auto agent : agents) {
+			pixels.set_pixel(agent.x + (width * agent.y), agent.color);
+		}
+
+		pixels.update();
+	}
+
+	void handle_user() {
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && steps_per_sec < 999) {
+			++steps_per_sec;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && steps_per_sec > 0) {
+			--steps_per_sec;
+		}
+	}
+
+	void add_grid(unsigned size, sf::Color color) {
+
+		grids.emplace_back(width, height, size, color);
+
+		for (auto &g : grids) {
+			g.setScale(pixel_size, pixel_size);
+		}
+	}
+
+	void set_dimentions(sf::Vector2u dim) {
+
+		width  = dim.x;
+		height = dim.y;
+
+		automaton.set_dimentions({width, height});
+
+		pixels.set_dimentions({width, height});
+
+		for (auto &g : grids) {
+			g.set_dimentions({width, height});
+		}
+	}
 };
 
 #endif // CAVIS_H
